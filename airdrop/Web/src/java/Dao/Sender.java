5e39d3e779d0f8b6ef1d3bc86a441fccf51f21b3
mk <93697650+mk0-0@users.noreply.github.com>
2022-06-12 16:18:29 +0800

    添加了PC端的控制台键盘输入

--- airdrop/Web/src/java/Dao/Sender.java
+++ airdrop/Web/src/java/Dao/Sender.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Sender {
 
@@ -14,11 +15,13 @@ public class Sender {
         while (true) {
             // 每当接收到客户端Socket的请求，服务端也对应产生一个Socket
             Socket s = ss.accept();
-            // 获得 socket对端的输出流
+            // 处理控制台输入并得到响应的类型数据
+            Scanner sc = new Scanner(System.in);
+            // 获得socket对端的输出流
             OutputStream os = s.getOutputStream();
-            // 向socket另一端发送一句 Hello
-            os.write("TestTest!\n".getBytes("utf-8"));
-            // 关闭输出流,关闭socket
+            // 向socket另一端发送字符串
+            os.write(sc.nextLine().getBytes("utf-8"));
+            // 关闭输出流，关闭socket
             os.close();
             s.close();
         }
